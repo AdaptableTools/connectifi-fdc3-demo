0b8263a0061b2a2ca1fb138a5c16c1c177ceb35e
Andrei Floricel <andrei.floricel@gmail.com>
2023-08-11 19:47:53 +0300

    First draft

--- src/AdaptableAgGrid/AdaptableAgGrid.tsx
+++ src/AdaptableAgGrid/AdaptableAgGrid.tsx
@@ -1,19 +1,37 @@
 import * as React from 'react';
 import { createRoot, Root } from 'react-dom/client';
-import { useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { GridOptions } from '@ag-grid-community/core';
 import { AgGridReact } from '@ag-grid-community/react';
 import AdaptableReact, {
   AdaptableApi,
   AdaptableOptions,
+  HandleFdc3IntentContext,
 } from '@adaptabletools/adaptable-react-aggrid';
 import { columnDefs, defaultColDef } from './columnDefs';
 import { TickerData, rowData } from './rowData';
 import { agGridModules } from './agGridModules';
+import { ConnectifiDesktopAgent, createAgent } from '@connectifi/agent-web';
 
 const renderWeakMap: WeakMap<HTMLElement, Root> = new WeakMap();
 
+const priceMap: Map<string, number> = new Map<string, number>();
+
 export const AdaptableAgGrid = () => {
+  const [fdc3Initialised, setFdc3Initialised] = useState<boolean>(false);
+
+  useEffect(() => {
+    if ((globalThis as any).fdc3 == null) {
+      createAgent('https://dev.connectifi-interop.com', 'adaptable@sandbox', {
+        logLevel: 'debug',
+      }).then((agent) => {
+        (globalThis as any).fdc3 = agent as ConnectifiDesktopAgent;
+        document.dispatchEvent(new CustomEvent('fdc3Ready', {}));
+        setFdc3Initialised(true);
+      });
+    }
+  }, []);
+
   const gridOptions = useMemo<GridOptions<TickerData>>(
     () => ({
       defaultColDef,
@@ -26,6 +44,7 @@ export const AdaptableAgGrid = () => {
     }),
     [],
   );
+
   const adaptableOptions = useMemo<AdaptableOptions<TickerData>>(
     () => ({
       licenseKey: import.meta.env.VITE_ADAPTABLE_LICENSE_KEY,
@@ -33,12 +52,143 @@ export const AdaptableAgGrid = () => {
       userName: 'AdaptableUser',
       adaptableId: 'AdaptableConnectifiPoc',
       adaptableStateKey: 'adaptable_connectifi_poc',
+      fdc3Options: {
+        enableFdc3: true,
+        enableLogging: true,
+        gridDataContextMapping: {
+          'fdc3.instrument': {
+            name: '_colId.Name',
+            id: {
+              ticker: '_colId.Symbol',
+            },
+          },
+        },
+        intents: {
+          raises: {
+            ViewInstrument: [
+              {
+                contextType: 'fdc3.instrument',
+                contextMenu: {
+                  columnIds: ['Name', 'Symbol'],
+                },
+              },
+            ],
+            ViewChart: [
+              {
+                contextType: 'fdc3.instrument',
+                actionButton: {
+                  id: 'viewChartBtn',
+                  tooltip: 'View Chart',
+                  icon: {
+                    name: 'chart',
+                  },
+                },
+              },
+            ],
+            ViewNews: [
+              {
+                contextType: 'fdc3.instrument',
+                actionButton: {
+                  id: 'viewNewsBtn',
+                  tooltip: 'View News',
+                  icon: {
+                    name: 'clipboard',
+                  },
+                },
+              },
+            ],
+          },
+        },
+        customIntents: {
+          raises: {
+            // TODO AFL: currently not possible because we can't handle IntentResults
+            // GetPrice: [
+            //   {
+            //     contextType: 'fdc3.instrument',
+            //     actionColumn: {
+            //       columnId: 'fdc3GetPriceColumn',
+            //       friendlyName: 'FDC3: Get Price Info',
+            //       button: {
+            //         id: 'GetPriceButton',
+            //         label: 'Get Price Info',
+            //         icon: {
+            //           name: 'quote',
+            //         },
+            //         tooltip: (button, context) => {
+            //           return `Get Price Info for ${context.rowData.Name}`;
+            //         },
+            //         buttonStyle: {
+            //           tone: 'info',
+            //           variant: 'outlined',
+            //         },
+            //       },
+            //     },
+            //   },
+            // ],
+          },
+        },
+        handleIntent: () => {},
+      },
+      actionColumnOptions: {
+        actionColumns: [
+          {
+            columnId: 'fdc3GetPriceColumn',
+            friendlyName: 'FDC3: Get Price Info',
+            actionColumnButton: {
+              label: (button, context) => {
+                const priceInfo = priceMap.get(context.data.Symbol);
+                return priceInfo ? `${priceInfo}` : 'Get Price Info';
+              },
+              onClick: (button, context) => {
+                const fdc3Api = context.adaptableApi.fdc3Api;
+                const { rowNode, data } = context;
+                const ticker = data.Symbol;
+                fdc3Api
+                  .raiseIntentFromRow(rowNode, 'GetPrice', 'fdc3.instrument')
+                  .then((result: any) => {
+                    priceMap.set(ticker, result?.price);
+                  });
+              },
+              icon: {
+                name: 'quote',
+              },
+              tooltip: (button, context) => {
+                return `Get Price Info for ${context.data.Name}`;
+              },
+              buttonStyle: (button, context) => {
+                const priceInfo = priceMap.get(context.data.Symbol);
+                return {
+                  tone: priceInfo ? 'success' : 'info',
+                  variant: 'outlined',
+                };
+              },
+            },
+          },
+        ],
+      },
       predefinedConfig: {
         Theme: {
           CurrentTheme: 'dark',
         },
         Dashboard: {
           DashboardTitle: 'Adaptable - Connectifi',
+        },
+        Layout: {
+          CurrentLayout: 'DefaultLayout',
+          Layouts: [
+            {
+              Name: 'DefaultLayout',
+              Columns: [
+                'Name',
+                'Sector',
+                'Symbol',
+                'fdc3GetPriceColumn',
+                'Contact',
+                'Email',
+                'fdc3ActionColumn',
+              ],
+            },
+          ],
         },
       },
     }),
@@ -49,29 +199,36 @@ export const AdaptableAgGrid = () => {
 
   return (
     <div className={'flex h-screen flex-col'}>
-      <AdaptableReact
-        className={'flex-none'}
-        gridOptions={gridOptions}
-        adaptableOptions={adaptableOptions}
-        renderReactRoot={(node, container) => {
-          let root = renderWeakMap.get(container);
-          if (!root) {
-            renderWeakMap.set(container, (root = createRoot(container)));
-          }
-          root.render(node);
-          return () => {
-            root?.unmount();
-          };
-        }}
-        onAdaptableReady={({ adaptableApi }) => {
-          // save a reference to adaptable api
-          adaptableApiRef.current = adaptableApi;
-          console.log('ready!!!');
-        }}
-      />
-      <div className="ag-theme-alpine flex-1">
-        <AgGridReact gridOptions={gridOptions} modules={agGridModules} />
-      </div>
+      {!fdc3Initialised ? (
+        <div>Initialising FDC3...</div>
+      ) : (
+        <>
+          {' '}
+          <AdaptableReact
+            className={'flex-none'}
+            gridOptions={gridOptions}
+            adaptableOptions={adaptableOptions}
+            renderReactRoot={(node, container) => {
+              let root = renderWeakMap.get(container);
+              if (!root) {
+                renderWeakMap.set(container, (root = createRoot(container)));
+              }
+              root.render(node);
+              return () => {
+                root?.unmount();
+              };
+            }}
+            onAdaptableReady={({ adaptableApi }) => {
+              // save a reference to adaptable api
+              adaptableApiRef.current = adaptableApi;
+              (window as any).api = adaptableApi;
+            }}
+          />
+          <div className="ag-theme-alpine flex-1">
+            <AgGridReact gridOptions={gridOptions} modules={agGridModules} />
+          </div>
+        </>
+      )}
     </div>
   );
 };
