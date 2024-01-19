aa24acb2be12a12372b5efb0f433c17dfc660d16
Andrei Floricel <andrei.floricel@gmail.com>
2024-01-19 18:00:44 +0200

    Rename params

--- src/createAgent.js
+++ src/createAgent.js
@@ -1,13 +1,13 @@
 import { createAgent } from 'https://dev.connectifi-interop.com/agent/main.bundle.js';
 
-const url =
+const INTEROP_HOST =
   import.meta.env.VITE_CONNECTIFI_INTEROP_HOST ||
   'https://dev.connectifi-interop.com';
-const sandbox = import.meta.env.VITE_CONNECTIFI_APPID || 'adaptable@sandbox';
-const logLevel = import.meta.env.VITE_CONNECTIFI_LOG_LEVEL || 'debug';
+const APP_ID = import.meta.env.VITE_CONNECTIFI_APPID || 'adaptable@sandbox';
+const LOG_LEVEL = import.meta.env.VITE_CONNECTIFI_LOG_LEVEL || 'debug';
 
-createAgent(url, sandbox, {
-  logLevel: logLevel,
+createAgent(INTEROP_HOST, APP_ID, {
+  logLevel: LOG_LEVEL,
 }).then((fdc3) => {
   window.fdc3 = fdc3;
   document.dispatchEvent(new CustomEvent('fdc3Ready', {}));
